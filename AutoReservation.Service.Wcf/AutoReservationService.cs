5e2abf2c81065f1bb5ff724aaa83260490088ec2
Lukas Koelbener <lkoelben@hsr.ch>
2014-12-01 15:44:54 +0100

    Stub Implementation of AutoReservationService

--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,13 +1,87 @@
-﻿using System;
+﻿using AutoReservation.Common.Interfaces;
+using AutoReservation.Common.DataTransferObjects;
+using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace AutoReservation.Service.Wcf
 {
-    public class AutoReservationService
+    public class AutoReservationService : IAutoReservationService
     {
-        private static void WriteActualMethod()
+
+        public List<AutoDto> GetAllAutos()
         {
-            Console.WriteLine("Calling: " + new StackTrace().GetFrame(1).GetMethod().Name);
+            throw new NotImplementedException();
+        }
+
+        public AutoDto getAuto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addAuto(AutoDto auto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateAuto(AutoDto original, AutoDto modified)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteAuto(AutoDto auto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<KundeDto> GetAllKunden()
+        {
+            throw new NotImplementedException();
+        }
+
+        public KundeDto getKunde(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addKunde(KundeDto kunde)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateKunde(KundeDto original, KundeDto modified)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteKunde(KundeDto kunde)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ReservationDto> GetAllReservationen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReservationDto getReservation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addReservation(ReservationDto reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateReservation(ReservationDto original, ReservationDto modified)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteReservation(ReservationDto auto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
