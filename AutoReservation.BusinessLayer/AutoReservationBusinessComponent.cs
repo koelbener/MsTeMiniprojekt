be769ec789b4e687c444fb58e6ff2f2e9d368fac
Lukas Koelbener <lkoelben@hsr.ch>
2014-12-01 16:53:35 +0100

    model implemented

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using AutoReservation.Dal;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
@@ -8,13 +9,120 @@ namespace AutoReservation.BusinessLayer
     public class AutoReservationBusinessComponent
     {
 
-        //private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
-        //{
-        //    var databaseValue = context.Entry(original).GetDatabaseValues();
-        //    context.Entry(original).CurrentValues.SetValues(databaseValue);
+        private AutoReservationEntities context;
 
-        //    throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
-        //}
+        public AutoReservationBusinessComponent()
+        {
+            context = new AutoReservationEntities();
+        }
+
+        public IList<Auto> getAutos()
+        {
+            return context.Autos.ToList();
+        }
+
+        public Auto getAuto(int id)
+        {
+            return context.Autos.SingleOrDefault(a => a.Id == id);
+        }
+
+        // TODO: Exception handling for all update methods
+        public void updateAuto(Auto original, Auto modified)
+        {
+            context.Autos.Attach(original);
+            context.Entry(original).CurrentValues.SetValues(modified);
+            context.SaveChanges();
+        }
+
+        public void deleteAuto(Auto auto)
+        {
+            context.Autos.Attach(auto);
+            context.Autos.Remove(auto);
+            context.SaveChanges();
+        }
+
+        public void addAuto(Auto auto)
+        {
+            context.Autos.Add(auto);
+            context.SaveChanges(); // TODO: SaveChanges always needed?
+        }
+
+
+        /**
+         * Kunden
+        */
+
+        public IList<Kunde> getKunden()
+        {
+            return context.Kunden.ToList();
+        }
+
+        public Kunde getKunde(int id)
+        {
+            return context.Kunden.SingleOrDefault(k => k.Id == id);
+        }        
+
+        public void deleteKunde(Kunde kunde)
+        {
+            context.Kunden.Attach(kunde);
+            context.Kunden.Remove(kunde);
+            context.SaveChanges();
+        }
+
+        public void updateKunde(Kunde original, Kunde modified)
+        {
+            context.Kunden.Attach(original);
+            context.Entry(original).CurrentValues.SetValues(modified);
+            context.SaveChanges();
+        }
+
+        public void addKunde(Kunde kunde)
+        {
+            context.Kunden.Add(kunde);
+            context.SaveChanges(); 
+        }
+
+
+        /**
+         *  Reservation
+        */ 
+        public IList<Reservation> getReservationen()
+        {
+            return context.Reservationen.ToList();
+        }
+
+        public Reservation getReservation(int reservationNr)
+        {
+            return context.Reservationen.SingleOrDefault(r => r.ReservationNr == reservationNr);
+        }        
+
+        public void deleteReservation(Reservation reservation)
+        {
+            context.Reservationen.Attach(reservation);
+            context.Reservationen.Remove(reservation);
+            context.SaveChanges();
+        }
+        public void updateReservation(Reservation original, Reservation modified)
+        {
+            context.Reservationen.Attach(original);
+            context.Entry(original).CurrentValues.SetValues(modified);
+            context.SaveChanges();
+        }
+
+        public void addReservation(Reservation reservation)
+        {
+            context.Reservationen.Add(reservation);
+            context.SaveChanges();
+        }
+
+
+        private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
+        {
+            var databaseValue = context.Entry(original).GetDatabaseValues();
+            context.Entry(original).CurrentValues.SetValues(databaseValue);
+
+            throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
+        }
 
     }
 }
