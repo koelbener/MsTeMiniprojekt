6af3ff94368ba6530da9186e99b0df9896ca2e90
emreavsar <emre.avsar92@gmail.com>
2014-12-11 15:52:30 +0100

    Added creator in UI, Locall and RemoteDataAccessCreator are not working atm
    See todo

---
+++ AutoReservation.Ui/Factory/Creator.cs
@@ -0,0 +1,19 @@
+﻿using AutoReservation.Ui.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoReservation.Ui.Factory
+{
+    abstract class Creator
+    {
+        public Creator GetCreator()
+        {
+            Type serviceLayerType = Type.GetType(Settings.Default.ServiceLayerType);
+            if (serviceLayerType == null) { return new LocalDataAccessCreator(); }
+            return (Creator)Activator.CreateInstance(serviceLayerType);
+        }
+    }
+}
